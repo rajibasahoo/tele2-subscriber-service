f1f6015fe9991ff16aabff6ee1826b3dedb54ee0
rajibasahoo <rajiba.sahoo@gmail.com>
2019-06-08 09:56:04 +0530

    FEZ assignment

--- subscription-restapi-springboot/src/test/java/com/tele2/SubscriptionTest.java
+++ subscription-restapi-springboot/src/test/java/com/tele2/SubscriptionTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
